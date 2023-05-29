277854d3424315cf4f5cfb025bba529d4f6a1892
Sourav Singh <commitsovercoffee@gmail.com>
2023-05-29 14:24:15 +0530

    adds lsp, cmp, auto-pair & indent guide support

--- init.lua
+++ init.lua
@@ -88,55 +88,85 @@ require("lazy").setup({
         'rmagatti/auto-session',
         config = function()
             require("auto-session").setup({
-            log_level = "error",
-            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-            session_lens = {
-                -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-                load_on_setup = true,
-                theme_conf = { border = true },
-                previewer = false,
+                log_level = "error",
+                auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+                session_lens = {
+                    -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+                    load_on_setup = true,
+                    theme_conf = { border = true },
+                    previewer = false,
                 },
             })
         end
     },
 
     -- syntax highlighting
-    { 'nvim-treesitter/nvim-treesitter',
-       build = ":TSUpdate",
-       config = function()
-           require("nvim-treesitter.configs").setup({
- 
-            ensure_installed = { "astro", "bash", "c", "css", "fish", "gdscript", "go", "html", "javascript", "json", "lua", "python", "regex", "sql", "svelte", "toml", "yaml" },
-            sync_install = true,
-            auto_install = true, -- requires `tree-sitter` cli
-            ignore_install = { "" },
-            highlight = {
-                enable = true,
-                disable = {},
-                -- disables slow treesitter highlight for large files
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "astro", "bash", "c", "css", "fish", "gdscript", "go", "html", "javascript", "json",
+                    "lua", "python", "regex", "sql", "svelte", "toml", "yaml" },
+                sync_install = true,
+                auto_install = true, -- requires `tree-sitter` cli
+                ignore_install = { "" },
+                highlight = {
+                    enable = true,
+                    disable = {},
+                    -- disables slow treesitter highlight for large files
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
                     end
-                end},
-           })
-       end},
+                },
+            })
+        end
+    },
 
-    -- lsp support
-    -- autocompletion
+    -- lsp support with auto-completion, auto-pairs and indentation guides.
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs to stdpath for Neovim
+            {
+                'williamboman/mason.nvim',
+                config = function()
+                    require('mason').setup()
+                end
+            },
+            'williamboman/mason-lspconfig.nvim',
+            { 'j-hui/fidget.nvim',     opts = {} },
+            'folke/neodev.nvim',
+
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-nvim-lsp',
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-calc",
+
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            { "windwp/nvim-autopairs", opts = {} },
+            {
+                "lukas-reineke/indent-blankline.nvim",
+                opts = {
+                    char = "|",
+                    show_trailing_blankline_indent = false,
+                },
+            }
+        },
+    }
+
     -- git integration
-
-    -- autopairs
-    -- indentation guides
 
     -- Development :
 
     -- neovim
     -- web
     -- go
-    --
 
 }, {})
 
@@ -149,7 +179,7 @@ vim.g.mapleader = "\\"
 vim.keymap.set('n', '<leader>ee', require('oil').open, {})
 
 -- Search for sessions
-vim.keymap.set("n", "<leader>ss", require("auto-session.session-lens").search_session, {noremap = true,})
+vim.keymap.set("n", "<leader>ss", require("auto-session.session-lens").search_session, { noremap = true, })
 
 -- Open telescope builtins
 local builtin = require('telescope.builtin')
@@ -224,3 +254,134 @@ vim.o.termguicolors = true
 
 -- Enable dark mode.
 vim.o.background = "dark"
+
+-- { Plugin Config } -----------------------------------------------------
+
+-- [ LSP ]
+
+local on_attach = function(_, bufnr)
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+
+    vim.api.nvim_set_keymap("n", "<Leader>zz", ":Format<CR>", { noremap = true, silent = true })
+end
+
+local servers = {
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+        },
+    },
+}
+
+require('neodev').setup()
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+    function(server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+        }
+    end,
+}
+
+-- [ Auto Completion  ]
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+-- Load snippets from VSCode
+require('luasnip.loaders.from_vscode').lazy_load()
+
+-- Configure luasnip
+luasnip.config.setup {}
+
+-- Setup nvim-cmp
+cmp.setup {
+    -- Configure snippet expansion behavior
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+
+    -- Configure key mappings
+    mapping = cmp.mapping.preset.insert {
+        -- Navigation mappings
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+        -- Completion and confirmation mappings
+        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+
+        -- Tab-based mappings for completion and snippet expansion
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+
+    -- Configure completion sources
+    sources = {
+        { name = 'nvim_lsp' }, -- Use nvim-lsp as a completion source
+        { name = 'luasnip' },  -- Use luasnip as a completion source
+        { name = "path" },     -- filesystem paths cmp
+        { name = "calc" },     -- math calculations cmp
+    },
+}
