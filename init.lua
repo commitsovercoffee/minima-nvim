7f468faaad4a139361200dd76e168732f1a798ce
commitsovercoffee <commitsovercoffee@gmail.com>
2024-07-30 19:54:39 +0530

    adds flash plugin

--- init.lua
+++ init.lua
@@ -180,12 +180,28 @@ require("lazy").setup({
 		end,
 	},
 
-	-- Visual multi cursor
+	-- Visual multi cursor.
 	{
 		"mg979/vim-visual-multi",
 		config = function()
 			require("vim-visual-multi").setup({})
 		end,
+	},
+
+	-- Search label based code navigation
+	{
+  		"folke/flash.nvim",
+  		event = "VeryLazy",
+  		---@type Flash.Config
+  		opts = {},
+  		-- stylua: ignore
+  		keys = {
+    			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  		},
 	},
 
 	-- Auto pairs braces
